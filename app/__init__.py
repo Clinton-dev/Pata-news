d42aecf2aa585ffa94767b1b1f4a893da7152155
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-01 23:12:22 +0300

    Add navigation

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from .config import DevConfig
-
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
-bootstrap = Bootstrap(app)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
