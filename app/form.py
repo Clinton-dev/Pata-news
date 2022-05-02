986acf6b2b4b6ee8b0f8659ce1dc9400a34da1b4
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-03 00:46:21 +0300

    Add search functionality

--- app/form.py
+++
@@ -1,7 +0,0 @@
-from flask import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms import DataRequired
-
-class SearchForm(FlaskForm):
-    newsname = StringField('news_query', validators=[DataRequired()])
-    submit = SubmitField('search')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, redirect
 from app import app
 from .request import get_news,get_articles, search_news
+
+app.config['SECRET_KEY'] = '3097c0d809dad8d3923f0afdb79f0e92'
 
 # Views
 @app.route('/')
@@ -11,7 +13,13 @@ def index():
     '''
     title="Pata news"
     source = get_news()
-    return render_template('index.html', title= title, sources=source)
+
+    search_news = request.args.get('news_query')
+
+    if search_news:
+        return redirect(url_for('search', news_name=search_news))
+    else:
+        return render_template('index.html', title= title, sources=source)
 
 @app.route('/articles/<article_id>')
 def article(article_id):
@@ -25,13 +33,13 @@ def article(article_id):
     return render_template('articles.html', title=title, articles=article)
 
 
-@app.route('/search')
+@app.route('/search/<news_name>')
 def search(news_name):
     """
     Display search results
     """
-    name_list = news_name.split(" ")
-    name_format = "+".join(name_list)
-    searched_news = search_news(name_format)
-    title= f"search results for {news_name}"
-    return render_template('search.html', news = searched_news)
+    news_name_list = news_name.split(" ")
+    news_name_format = "+".join(news_name_list)
+    searched_news = search_news(news_name_format)
+    title = f'search results for {news_name}'
+    return render_template('search.html',news = searched_news)
